357b39aef005dd16d44f5bfecd6e8abeea716351
mlodypies <simsd1234@gmail.com>
2023-05-17 01:27:27 +0200

    working initial routing

--- pub/index.php
+++ pub/index.php
@@ -1,26 +1,12 @@
 <?php
-require('./../src/config.php');
 
+require_once('./../src/config.php');
 
+use Steampixel\Route;
+
+Route::add('/', function() {
+    echo "Strona glowna";
+}); 
+
+Route::run('/cms/pub');
 ?>
-
-<form action="" method="post" enctype="multipart/form-data">
-        <label for="uploadedFileInput">
-            Wybierz plik do wgrania na serwer:
-        </label>
-       <input type="file" name="uploadedFile" id="uploadedFileInput" required>
-       <input type="submit" value="Wyslij plik" name="submit">
-    </form>
-
-    <?php
-    if(isset($_POST['submit'])) {
-         Post::upload($_FILES['uploadedFile']['tmp_name']);
-    }
-
-    ?>
-
-    Ostatni post:
-    <pre>
-    <?php
-    var_dump(Post::getPage());
-    ?>
