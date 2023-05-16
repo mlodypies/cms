c49bfc3696f7b5235cef4f11d2d292e071365cf3
mlodypies <simsd1234@gmail.com>
2023-05-17 01:44:24 +0200

    implementacja twiga

--- pub/index.php
+++ pub/index.php
@@ -5,7 +5,15 @@ require_once('./../src/config.php');
 use Steampixel\Route;
 
 Route::add('/', function() {
-    echo "Strona glowna";
+    //strona wyswietlajaca obrazki
+    global $twig;
+    $twig->display("index.html.twig");
+}); 
+
+Route::add('/upload', function() {
+    //strona z formularzem do wgrywania obrazkow
+    global $twig;
+    $twig->display("upload.html.twig");
 }); 
 
 Route::run('/cms/pub');
